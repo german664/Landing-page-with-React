408c0eac50a92d0dfc7553547e7843a5666f1ea4
German Mora <german.mora.664@gmail.com>
2020-08-11 13:35:36 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1 className="text-danger"> Prueba </h1>
+        <i className="fas fa-globe-europe"> </i>
+      </div>
+      <div className="App">
+        <h1 className="text-danger"> Prueba </h1>
+        <i className="fas fa-globe-europe"> </i>
+      </div>
+    </>
+  );
+}
+
+export default App;
